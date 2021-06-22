672e436f4739cd35c4d4dffcd8a07face2992912
akthrms <akt.hrms@gmail.com>
2021-06-23 02:10:05 +0900

    update

--- chapter05/src/Chapter05.hs
+++ chapter05/src/Chapter05.hs
@@ -1,5 +1,15 @@
 module Chapter05 where
 
+import Control.Applicative (Alternative ((<|>)))
+import Control.Monad (forM_, guard)
+import qualified Control.Monad.Trans.Except as EX
+import Control.Monad.Trans.Reader
+  ( Reader,
+    ask,
+    asks,
+    local,
+    runReader,
+  )
 import Control.Monad.Trans.State (State, get, put, runState)
 import Data.List (sort)
 import System.Random.Shuffle (shuffleM)
@@ -103,3 +113,119 @@ runGame :: IO ()
 runGame = do
   deck <- shuffleM [1 .. 50]
   print $ runState gameWithState deck
+
+-- 5.2.1
+
+run1 :: IO ()
+run1 = do
+  forM_ [1 .. 9] $ \x -> do
+    forM_ [1 .. 9] $ \y -> do
+      putStr $ show (x * y) ++ "\t"
+    putStrLn ""
+
+-- 5.3.2
+
+data Gender = Man | Woman deriving (Show)
+
+data Human = Human {name :: String, age :: Int, gender :: Gender} deriving (Show)
+
+run2 :: IO ()
+run2 = do
+  print $ Human "Taro" 10 Man
+  print $ Human <$> Just "Taro" <*> pure 10 <*> pure Man
+  print $ Human <$> Just "Taro" <*> Nothing <*> pure Man
+
+-- 5.3.3
+
+assocs :: [(String, Integer)]
+assocs = [("hiratara", 39), ("shu1", 0), ("masaharu", 32)]
+
+run3 :: IO ()
+run3 = do
+  print $ lookup "homma" assocs <|> lookup "hiratara" assocs
+  print $ do
+    age <- lookup "homma" assocs <|> lookup "hiratara" assocs
+    guard $ age < 20
+    pure age
+
+-- 5.4.1
+
+safeDiv :: Integer -> Integer -> Maybe Integer
+safeDiv k n
+  | n == 0 = Nothing
+  | otherwise = Just $ k `div` n
+
+calc :: Integer -> Maybe Integer
+calc n = do
+  x <- 100 `safeDiv` n
+  -- y <- 100 `safeDiv` (x - 1)
+  -- pure y
+  100 `safeDiv` (x - 1)
+
+safeDiv' :: Integer -> Integer -> Either String Integer
+safeDiv' k n
+  | n == 0 = Left $ "Illegal division by zero. k: " ++ show k
+  | otherwise = Right $ k `div` n
+
+calc' :: Integer -> Either String Integer
+calc' n = do
+  x <- 100 `safeDiv'` n
+  100 `safeDiv'` (x - 1)
+
+-- >>> calc' 50
+-- Right 100
+-- >>> calc' 0
+-- Left "Illegal division by zero. k: 100"
+
+-- 5.4.2
+
+safeDiv'' :: Integer -> Integer -> EX.Except String Integer
+safeDiv'' k n
+  | n == 0 = EX.throwE $ "Illegal division by zero. k: " ++ show k
+  | otherwise = pure $ k `div` n
+
+calc'' :: Integer -> Either String Integer
+calc'' n = EX.runExcept $ do
+  EX.catchE
+    ( do
+        x <- 100 `safeDiv''` n
+        100 `safeDiv''` (x - 1)
+    )
+    (\_ -> pure 0)
+
+-- 5.5.1
+
+run4 :: IO ()
+run4 = print $ runReader readRound 1.5013232
+
+readRound :: Reader Double Int
+readRound = do round <$> ask
+
+data PowerEnv = PowerEnv {powerEnergy :: !Double, powerSaveMode :: !Bool}
+
+consume :: Reader PowerEnv Double
+consume = do
+  energy <- asks powerEnergy
+  saveMode <- asks powerSaveMode
+  let consumption = if saveMode then energy / 10.0 else energy
+  pure consumption
+
+run5 :: IO ()
+run5 = print $ runReader consume $ PowerEnv 10.0 True
+
+testRun :: PowerEnv -> Double
+testRun env = (`runReader` env) $ do
+  cons1 <- consume
+  cons2 <- consume
+  consOther <- local (\e -> e {powerSaveMode = True}) $ do
+    cons3 <- consume
+    cons4 <- consume
+    pure $ cons3 + cons4
+  pure $ cons1 + cons2 + consOther
+
+-- >>> testRun $ PowerEnv 100.0 False
+-- 220.0
+-- >>> testRun $ PowerEnv 80.0 False
+-- 176.0
+-- >>> testRun $ PowerEnv 100.0 True
+-- 40.0
