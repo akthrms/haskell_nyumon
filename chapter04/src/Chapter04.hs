0b84bd50d49155ce6e67023407da73d57a340699
akthrms <akt.hrms@gmail.com>
2021-06-24 02:50:47 +0900

    update

--- chapter04/src/Chapter04.hs
+++ chapter04/src/Chapter04.hs
@@ -37,6 +37,8 @@ import System.IO
     openFile,
   )
 
+-- 4.1.3
+
 run :: IO ()
 run =
   getLine >>= \x ->
@@ -54,23 +56,29 @@ run3 :: IO ()
 run3 = do
   x <- getLine
   putStrLn $ "1つ目の入力: " ++ x
-  getLine >>= pure . ("2つ目の入力: " ++) >>= putStrLn
+  -- getLine >>= pure . ("2つ目の入力: " ++) >>= putStrLn
+  getLine >>= putStrLn . ("2つ目の入力: " ++)
+
+-- 4.2.1
 
 run4 :: IO ()
 run4 = do
   args <- getArgs
   print args
 
+-- 4.3.3
+
 run5 :: IO ()
 run5 = do
-  xs <- getContents >>= pure . lines
-  counter 0 xs
+  -- operations <- getContents >>= pure . lines
+  operations <- lines <$> getContents
+  counter 0 operations
 
 counter :: Int -> [String] -> IO ()
 counter _ [] = pure ()
-counter i ("up" : xs) = print (i + 1) >> counter (i + 1) xs
-counter i ("down" : xs) = print (i - 1) >> counter (i - 1) xs
-counter i (_ : xs) = counter i xs
+counter i ("up" : operations) = print (i + 1) >> counter (i + 1) operations
+counter i ("down" : operations) = print (i - 1) >> counter (i - 1) operations
+counter i (_ : operations) = counter i operations
 
 interact' :: (String -> String) -> IO ()
 interact' f = do
@@ -80,118 +88,105 @@ interact' f = do
 run6 :: IO ()
 run6 = interact' $ map toUpper
 
+-- 4.3.4
+
 run7 :: IO ()
-run7 = readFile "sample.txt" >>= putStrLn
-
-run8 :: IO ()
-run8 = readFile "sample.txt" >>= putStrLn . reverse
-
-run9 :: IO ()
-run9 = do
-  s <- readFile "sample.txt"
-  putStrLn $ take 5 s
-  writeFile "sample.txt" "Hello, Lazy IO!"
-
-run10 :: IO ()
-run10 = do
-  h <- openFile "sample.txe" ReadMode
-  loop 0 h
-  hClose h
+run7 = do
+  handle <- openFile "sample.txe" ReadMode
+  loop 0 handle
+  hClose handle
   where
-    loop i h = do
-      eof <- hIsEOF h
-      if not eof
+    loop i handle = do
+      isEof <- hIsEOF handle
+      if not isEof
         then do
-          s <- hGetLine h
+          s <- hGetLine handle
           putStrLn $ show i ++ ":" ++ s
-          loop (i + 1) h
+          loop (i + 1) handle
         else pure ()
 
-run11 :: IO ()
-run11 = do
+-- 4.3.5
+
+run8 :: IO ()
+run8 = do
   bs <- B.readFile "sample"
   B.writeFile "sample" . B.map complement $ bs
 
-run12 :: IO ()
-run12 = getPermissions "sample.txt" >>= setPermissions "sample.txt" . toReadableAndWritable
+-- 4.4.2
+
+run9 :: IO ()
+run9 = getPermissions "sample.txt" >>= setPermissions "sample.txt" . toReadableAndWritable
 
 toReadableAndWritable :: Permissions -> Permissions
 toReadableAndWritable permissions = permissions {readable = True, writable = True}
 
+run10 :: IO ()
+run10 = do
+  currentDirectory <- getCurrentDirectory
+  findFiles [currentDirectory ++ "/..", currentDirectory] "target.txt" >>= print
+  findFile [currentDirectory ++ "/..", currentDirectory] "target.txt" >>= print
+
+run11 :: IO ()
+run11 = do
+  currentDirectory <- getCurrentDirectory
+  findFilesWith checkWritable [currentDirectory ++ "/..", currentDirectory] "target.txt" >>= print
+  where
+    -- checkWritable filePath = getPermissions filePath >>= pure . writable
+    checkWritable filePath = writable <$> getPermissions filePath
+
+-- 4.5.1
+
+readDummyFile :: IO ()
+readDummyFile = readFile "dummy.txt" >>= putStrLn
+
+putErrorMessage :: SomeException -> IO ()
+putErrorMessage e = putStrLn $ "readFile failure!!!: " ++ displayException (e :: SomeException)
+
+run12 :: IO ()
+run12 = readDummyFile `catch` putErrorMessage
+
+putErrorMessage' :: SomeException -> IO ()
+putErrorMessage' (e :: SomeException) = putStrLn $ "readFile failure!!!: " ++ displayException e
+
 run13 :: IO ()
-run13 = do
-  current <- getCurrentDirectory
-  findFiles [current ++ "/..", current] "target.txt" >>= print
-  findFile [current ++ "/..", current] "target.txt" >>= print
+run13 = readDummyFile `catch` putErrorMessage'
 
 run14 :: IO ()
-run14 = do
-  current <- getCurrentDirectory
-  findFilesWith checkWritable [current ++ "/..", current] "target.txt" >>= print
-
-checkWritable :: FilePath -> IO Bool
-checkWritable filePath = getPermissions filePath >>= pure . writable
-
-run15 :: IO ()
-run15 =
-  (readFile "dummy.txt" >>= putStrLn)
-    `catch` ( \e ->
-                putStrLn $ "readFile failure!!!: " ++ displayException (e :: SomeException)
-            )
-
-run16 :: IO ()
-run16 =
-  (readFile "dummy.txt" >>= putStrLn)
-    `catch` ( \(e :: SomeException) ->
-                putStrLn $ "readFile failure!!!: " ++ displayException e
-            )
-
-run17 :: IO ()
-run17 =
-  (readFile "dummy.txt" >>= putStrLn)
-    `catch` ( \(e :: SomeException) ->
-                putStrLn $ "readFile failure!!!: " ++ displayException e
-            )
-    `finally` (putStrLn "finalization")
+run14 = readDummyFile `catch` putErrorMessage' `finally` putStrLn "finalization"
 
 catchZeroDiv :: ArithException -> IO Int
 catchZeroDiv DivideByZero = pure 0
 catchZeroDiv e = throwIO e
 
-run18 :: IO ()
-run18 = do
-  n1 <- (pure $ 100 `div` 0) `catch` catchZeroDiv
-  print n1
-  n2 <- (pure $! 100 `div` 0) `catch` catchZeroDiv
-  print n2
+-- >>> (pure $ 100 `div` 0) `catch` catchZeroDiv
+-- divide by zero
+-- >>> (pure $! 100 `div` 0) `catch` catchZeroDiv
+-- 0
 
 someOperation :: IO ()
 someOperation = undefined
 
-run19 :: IO ()
-run19 =
-  someOperation
-    `catch` ( \(e :: ArithException) ->
-                putStrLn $ "Catch ArithException: " ++ displayException e
-            )
-    `catch` ( \(e :: SomeException) ->
-                putStrLn $ "Catch SomeException: " ++ displayException e
-            )
+catchArithException :: ArithException -> IO ()
+catchArithException (e :: ArithException) = putStrLn $ "Catch ArithException: " ++ displayException e
 
-run20 :: IO ()
-run20 =
-  someOperation
-    `catches` [ Handler $ \(e :: ArithException) ->
-                  putStrLn $ "Catch ArithException: " ++ displayException e,
-                Handler $ \(e :: SomeException) ->
-                  putStrLn $ "Catch SomeException: " ++ displayException e
-              ]
+catchSomeException :: SomeException -> IO ()
+catchSomeException (e :: SomeException) = putStrLn $ "Catch SomeException: " ++ displayException e
 
-run21 :: IO ()
-run21 =
-  bracket (openFile "dummy.txt" ReadMode) hClose $ \h -> do
-    s <- hGetContents h
+run15 :: IO ()
+run15 = someOperation `catch` catchArithException `catch` catchSomeException
+
+run16 :: IO ()
+run16 = someOperation `catches` [Handler catchArithException, Handler catchSomeException]
+
+-- 4.5.2
+
+run17 :: IO ()
+run17 =
+  bracket (openFile "dummy.txt" ReadMode) hClose $ \handle -> do
+    s <- hGetContents handle
     putStrLn s
+
+-- 4.5.3
 
 data MyException
   = FirstError
@@ -209,8 +204,8 @@ throwMyException 1 = throwIO FirstError
 throwMyException 2 = throwIO SecondError
 throwMyException n = pure $ "Value = " ++ show n
 
-run22 :: IO ()
-run22 = do
+run18 :: IO ()
+run18 = do
   (throwMyException 1 >>= putStrLn) `catch` printMyException
   (throwMyException 2 >>= putStrLn) `catch` printMyException
   (throwMyException 3 >>= putStrLn) `catch` printMyException
