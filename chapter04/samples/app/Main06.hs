8fa911c92f5154cdb1e8443f6e1312e079868552
akthrms <akt.hrms@gmail.com>
2021-12-28 00:52:17 +0900

    update

---
+++ chapter04/samples/app/Main06.hs
@@ -0,0 +1,23 @@
+module Main06 where
+
+import System.Directory
+
+-- 4.4.2
+
+main :: IO ()
+main = getPermissions "sample.txt" >>= setPermissions "sample.txt" . toReadAndWritable
+
+toReadAndWritable :: Permissions -> Permissions
+toReadAndWritable p = p {readable = True, writable = True}
+
+main' :: IO ()
+main' = do
+  current <- getCurrentDirectory
+  findFiles [current ++ "/..", current] "target.txt" >>= print
+  findFile [current ++ "/..", current] "target.txt" >>= print
+
+main'' :: IO ()
+main'' = do
+  current <- getCurrentDirectory
+  let checkWritable filePath = getPermissions filePath >>= pure . writable
+  findFilesWith checkWritable [current ++ "/..", current] "target.txt" >>= print
