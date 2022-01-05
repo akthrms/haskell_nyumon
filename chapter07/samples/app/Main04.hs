49e323114e45140d0ff804a17fb477ba5bb3fe4b
akthrms <akt.hrms@gmail.com>
2022-01-05 23:46:45 +0900

    update

--- chapter07/samples/app/Main04.hs
+++ chapter07/samples/app/Main04.hs
@@ -4,8 +4,41 @@ module Main04 where
 
 -- 7.5.2
 
-import Data.Attoparsec.Text (decimal, feed, parse)
+import Control.Applicative (Alternative ((<|>)))
+import Data.Attoparsec.Text
+  ( Parser,
+    char,
+    decimal,
+    feed,
+    parse,
+    string,
+  )
+
+twoOfDecimal :: Parser (Int, Int)
+twoOfDecimal = do
+  left <- decimal
+  char ','
+  right <- decimal
+  pure (left, right)
+
+parens :: Parser a -> Parser a
+parens parser = do
+  char '('
+  res <- parser
+  char ')'
+  pure res
+
+data Animal = Dog | Pig deriving (Show)
+
+animal :: Parser Animal
+animal = (string "Dog" >> pure Dog) <|> (string "Pig" >> pure Pig)
 
 main :: IO ()
 main = do
   print (parse decimal "1000" `feed` "")
+
+  print (parse (parens twoOfDecimal) "(123,456)" `feed` "")
+
+  print (parse animal "Dog" `feed` "")
+  print (parse animal "Pig" `feed` "")
+  print (parse animal "Cat" `feed` "")
